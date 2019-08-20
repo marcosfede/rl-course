6df0701ba98d4d88929fda2731280d91ceffd6bf
Federico Marcos <federico.marcos@celerative.com>
2019-08-19 23:55:50 -0300

    some stuff

--- lecture2/src/main.rs
+++ lecture2/src/main.rs
@@ -1,40 +1,26 @@
-struct State<'a> {
-    name: String,
-    edges: Vec<Edge<'a>>,
+use mdp::{MDP, ActionOutcome, StateData, ActionData};
+
+random_policy(s: StateData) -> usize {
+    
 }
 
-struct Edge<'a> {
-    to: &'a State<'a>,
-    reward: isize,
-}
-
-const GAMMA: usize = 1;
-const STATES: [&str; 4] = ["Facebook", "Class1", "Class2", "Class3"];
 fn main() {
-    let mut Facebook = State {
-        name: String::from("Facebook"),
-        edges: Vec::new(),
-    };
-    let Class1 = State {
-        name: String::from("Class1"),
-        edges: Vec::new(),
-    };
-    let Class2 = State {
-        name: String::from("Class2"),
-        edges: Vec::new(),
-    };
-    let Class3 = State {
-        name: String::from("Class3"),
-        edges: Vec::new(),
-    };
-    Facebook.edges.push(Edge {to: &Facebook, reward: -1});
-    Facebook.edges.push(Edge {to: &Class1, reward: 0});
-    // Facebook.edges.push(Edge {to: &Facebook, reward: -1});
-    let REWARDS: [Vec<isize>; 4] = [vec![-1, 0], vec![-2], vec![0, -2], vec![1, 10]];
-    let ACTIONS: [Vec<&str>; 4] = [
-        vec!["Facebook", "Quit"],
-        vec!["Study"],
-        vec!["Sleep", "Study"],
-        vec!["Pub", "Study"],
-    ];
+    let mut g = MDP::new();
+    let facebook = g.add_state("Facebook");
+    let class1 = g.add_state("Class1");
+    let class2 = g.add_state("Class2");
+    let class3 = g.add_state("Class3");
+    let sleep = g.add_state("Sleep");
+    g.add_action(facebook, vec!(ActionOutcome::new(facebook)), -1.0);
+    g.add_action(facebook, vec!(ActionOutcome::new(class1)), 0.0);
+    g.add_action(class1, vec!(ActionOutcome::new(facebook)), -1.0);
+    g.add_action(class1, vec!(ActionOutcome::new(class2)), -2.0);
+    g.add_action(class2, vec!(ActionOutcome::new(sleep)), 0.0);
+    g.add_action(class2, vec!(ActionOutcome::new(class3)), -2.0);
+    g.add_action(class3, vec!(ActionOutcome::new(sleep)), 10.0);
+    g.add_action(class3, vec!(ActionOutcome::new_with_prob(class1, 0.2)), 1.0);
+    g.add_action(class3, vec!(ActionOutcome::new_with_prob(class2, 0.4)), 1.0);
+    g.add_action(class3, vec!(ActionOutcome::new_with_prob(class3, 0.4)), 1.0);
+
+    g.step(class1, policy)    
 }
---
+++ mdp/src/lib.rs
@@ -0,0 +1,96 @@
+pub type StateIndex = usize;
+
+pub struct StateData {
+    label: &'static str,
+    first_outgoing_edge: Option<ActionIndex>,
+}
+
+pub type ActionIndex = usize;
+
+pub struct ActionOutcome {
+    pub state: StateIndex,
+    pub probability: f64,
+}
+
+impl ActionOutcome {
+    pub fn new(state: StateIndex) -> ActionOutcome {
+        ActionOutcome {
+            state,
+            probability: 1.0,
+        }
+    }
+    pub fn new_with_prob(state: StateIndex, probability: f64) -> ActionOutcome {
+        ActionOutcome { state, probability }
+    }
+}
+
+pub struct ActionData {
+    outcomes: Vec<ActionOutcome>,
+    next_outgoing_edge: Option<ActionIndex>,
+    reward: f64,
+}
+
+pub struct MDP {
+    nodes: Vec<StateData>,
+    edges: Vec<ActionData>,
+}
+
+pub struct Actions<'graph> {
+    mdp: &'graph MDP,
+    current_edge_index: Option<ActionIndex>,
+}
+
+impl<'graph> Iterator for Actions<'graph> {
+    type Item = &'graph ActionData;
+    fn next(&mut self) -> Option<&'graph ActionData> {
+        match self.current_edge_index {
+            None => None,
+            Some(edge_num) => {
+                let edge = &self.mdp.edges[edge_num];
+                self.current_edge_index = edge.next_outgoing_edge;
+                Some(edge)
+            }
+        }
+    }
+}
+
+impl MDP {
+    pub fn new() -> MDP {
+        MDP {
+            nodes: Vec::new(),
+            edges: Vec::new(),
+        }
+    }
+
+    pub fn add_state(&mut self, label: &'static str) -> StateIndex {
+        let index = self.nodes.len();
+        self.nodes.push(StateData {
+            label,
+            first_outgoing_edge: None,
+        });
+        index
+    }
+
+    pub fn add_action(&mut self, source: StateIndex, outcomes: Vec<ActionOutcome>, reward: f64) {
+        let edge_index = self.edges.len();
+        let node_data = &mut self.nodes[source];
+        self.edges.push(ActionData {
+            outcomes: outcomes,
+            next_outgoing_edge: node_data.first_outgoing_edge,
+            reward,
+        });
+        node_data.first_outgoing_edge = Some(edge_index);
+    }
+
+    pub fn actions(&self, source: StateIndex) -> Actions {
+        let first_outgoing_edge = self.nodes[source].first_outgoing_edge;
+        Actions {
+            mdp: self,
+            current_edge_index: first_outgoing_edge,
+        }
+    }
+}
+
+pub trait Policy {
+    fn choose_action(s: StateData) -> ActionData;
+}
